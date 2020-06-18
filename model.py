9ace7aeaee2d110f7276c3feee88aa1b47da0918
Amber Chan <amber.chan@berkeley.edu>
2020-06-18 23:00:08 +0000

    Created Board model

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class Game(db.Model):
     """Game model for a game."""
 
-    __tablename__ = "game"
+    __tablename__ = "games"
 
     game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     player1 = db.Column(db.String(50), nullable=False)
@@ -16,3 +16,15 @@ class Game(db.Model):
     game_card = db.Column(db.String(100), nullable=False)
     words = db.Column(db.Text, nullable=False)
     game_status = db.Column(db.String(100), nullable=False)
+
+class Board(db.Model):
+    """Board model for board status of a game."""
+
+    __tablename__ = "boards"
+
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    game_id = db.Column(db.Integer, db.ForeignKey("games.game_id"), nullable=False)
+    location = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+
+    games = db.relationship("Game", backref="boards")
