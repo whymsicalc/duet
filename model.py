10130be0798ec2d3fa8c82bea80158fae3b1af73
Amber Chan <amber.chan@berkeley.edu>
2020-06-18 22:22:11 +0000

    Created Game model

--- model.py
+++ model.py
@@ -1,5 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 #Instantiate a SQLAlchemy object to create db.Model classes
 db = SQLAlchemy()
+
+class Game(db.Model):
+    """Game model for a game."""
+
+    __tablename__ = "game"
+
+    game_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    player1 = db.Column(db.String(50), nullable=False)
+    player2 = db.Column(db.String(50), nullable=False)
+    created_time = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    game_card = db.Column(db.String(100), nullable=False)
+    words = db.Column(db.Text, nullable=False)
+    game_status = db.Column(db.String(100), nullable=False)
