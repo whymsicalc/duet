df879d5880624d75e77cc936966275ba70414082
Amber Chan <amber.chan@berkeley.edu>
2020-07-10 01:18:15 +0000

    Created route to display homepage

--- server.py
+++ server.py
@@ -6,5 +6,10 @@ BOARDS = []
 
 WORDS = []
 
+@app.route("/")
+def homepage():
+    """Show homepage."""
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
