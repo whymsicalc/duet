12886c8cfe2e7e3ea1dc7755bb738942e0bf1dbd
Amber Chan <amber.chan@berkeley.edu>
2020-07-10 01:24:25 +0000

    Created route to take in information from homepage form and send user to game page

--- server.py
+++ server.py
@@ -11,5 +11,11 @@ def homepage():
     """Show homepage."""
     return render_template("index.html")
 
+@app.route("/create-game", methods=["POST"])
+def create_game():
+    """Create game for given user."""
+    player1 = request.form.get("player1")
+    return render_template("game_page.html", player1=player1)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
