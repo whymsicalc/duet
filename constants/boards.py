b1025e37bc3f00ee4856c7be066d24c49ba30c42
Amber Chan <amberchan@Ambers-MBP.kintsu.local>
2020-08-13 16:14:23 -0700

    Added first board to master list of boards to use for game (will need to add more later)

---
+++ constants/boards.py
@@ -0,0 +1,24 @@
+from constants.states import State
+
+BOARDS = [
+([State.NEUTRAL, State.GREEN, State.BLACK, State.NEUTRAL, State.NEUTRAL,
+    State.NEUTRAL, State.NEUTRAL, State.GREEN, State.GREEN, State.GREEN,
+    State.NEUTRAL, State.NEUTRAL, State.NEUTRAL, State.BLACK, State.GREEN,
+    State.NEUTRAL, State.BLACK, State.NEUTRAL, State.GREEN, State.GREEN,
+    State.NEUTRAL, State.NEUTRAL, State.GREEN, State.NEUTRAL, State.GREEN], 
+[State.GREEN, State.NEUTRAL, State.NEUTRAL, State.NEUTRAL, State.GREEN,
+    State.NEUTRAL, State.BLACK, State.GREEN, State.NEUTRAL, State.GREEN,
+    State.NEUTRAL, State.NEUTRAL, State.NEUTRAL, State.GREEN, State.BLACK,
+    State.BLACK, State.NEUTRAL, State.GREEN, State.NEUTRAL, State.NEUTRAL,
+    State.GREEN, State.NEUTRAL, State.NEUTRAL, State.GREEN, State.GREEN]),
+([State.NEUTRAL, State.GREEN, State.BLACK, State.NEUTRAL, State.NEUTRAL,
+    State.NEUTRAL, State.NEUTRAL, State.GREEN, State.GREEN, State.GREEN,
+    State.NEUTRAL, State.NEUTRAL, State.NEUTRAL, State.BLACK, State.GREEN,
+    State.NEUTRAL, State.BLACK, State.NEUTRAL, State.GREEN, State.GREEN,
+    State.NEUTRAL, State.NEUTRAL, State.GREEN, State.NEUTRAL, State.GREEN], 
+[State.GREEN, State.NEUTRAL, State.NEUTRAL, State.NEUTRAL, State.GREEN,
+    State.NEUTRAL, State.BLACK, State.GREEN, State.NEUTRAL, State.GREEN,
+    State.NEUTRAL, State.NEUTRAL, State.NEUTRAL, State.GREEN, State.BLACK,
+    State.BLACK, State.NEUTRAL, State.GREEN, State.NEUTRAL, State.NEUTRAL,
+    State.GREEN, State.NEUTRAL, State.NEUTRAL, State.GREEN, State.GREEN])
+]
