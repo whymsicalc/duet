89030d1773a65a0dceaece7e210aa89a46317374
Amber Chan <amberchan@Ambers-MBP.kintsu.local>
2020-08-13 17:43:08 -0700

    Created pairs of word and game card state for player1 to send to game page html page.

--- server.py
+++ server.py
@@ -21,7 +21,10 @@ def create_game():
     # save board index instead of the entire board and index into board
     # save into database
     # game link - game PK
-    return render_template("game_page.html", player1=player1, game_words=game_words, game_board=game_board)
+    pairs = []
+    for i in range(25):
+        pairs.append((game_words[i], game_board[0][i]))
+    return render_template("game_page.html", player1=player1, game_words=game_words, game_board=game_board, pairs=pairs)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
